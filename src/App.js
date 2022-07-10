03e06fb2549dce7f16143f01edcb9c33d24c16e0
Dima <dmitry5003@gmail.com>
2022-07-10 16:21:31 +0300

    link fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import AddRoom from './components/smarthouse/AddRoom';
-import { HashRouter as Router,Link,  Route, Routes } from 'react-router-dom';
-//import { BrowserRouter as Router,Link,  Route, Routes } from 'react-router-dom';
+//import { HashRouter, Link, Route, Routes } from 'react-router-dom';
+import {Link,  Route, Routes } from 'react-router-dom';
 import Home from './components/smarthouse/Home';
 import Room from './components/smarthouse/Room';
 import Sudoku from './components/sudoku/Sudoku'
@@ -13,20 +13,20 @@ function App() {
 
   return (
     <>
-    <nav className='navbar'>
-      <Link to={`/smarthouse`}>Smart House</Link>
-      <Link to={`sudoku`} > Sudoku</Link>
-      <Link >Market Shop</Link>
-    </nav>
-        <Routes>
-          <Route defoult exact path='/smarthouse' element={<Home roomsData={roomsData}/>} />
-          <Route exact path='/smarthouse/addRoom' element={<AddRoom roomsData={roomsData} setRoomsData={setRoomsData}/>} />
-          <Route exact path='/smarthouse/room' element={<Room roomsData={roomsData} setRoomsData={setRoomsData}/>} >
-            <Route exact path=':data' element={<Room />} />
-          </Route>
-          <Route exact path='sudoku' element={<Sudoku/>}/>
-          <Route path="*" element={<main style={{ padding: "1rem" }}><h3>How did you get here?<br /><br /> never mind</h3><h3>There's nothing here<br /><br /><Link to={`/smarthouse`} style={{textShadow:`1px 1px 1px gray`}}> go home!</Link></h3></main>} />
-        </Routes>
+      <nav className='navbar'>
+        <Link to={`/smarthouse`}>Smart House</Link>
+        <Link to={`/sudoku`} > Sudoku</Link>
+        {/* <Link >Market Shop</Link> */}
+      </nav>
+      <Routes>
+        <Route defoult exact path='/smarthouse' element={<Home roomsData={roomsData} />} />
+        <Route exact path='/smarthouse/addRoom' element={<AddRoom roomsData={roomsData} setRoomsData={setRoomsData} />} />
+        <Route exact path='/smarthouse/room' element={<Room roomsData={roomsData} setRoomsData={setRoomsData} />} >
+          <Route exact path=':data' element={<Room />} />
+        </Route>
+        <Route exact path='/sudoku' element={<Sudoku />} />
+        <Route path="*" element={<main style={{ padding: "1rem" }}><h3>How did you get here?<br /><br /> never mind</h3><h3>There's nothing here<br /><br /><Link to={`/smarthouse`} style={{ textShadow: `1px 1px 1px gray` }}> go home!</Link></h3></main>} />
+      </Routes>
     </>
   );
 }
